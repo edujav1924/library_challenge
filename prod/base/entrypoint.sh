342f44a6599e2705b696853db298700d017729a9
edgonzalez <edgonzalez@chacomer.com.py>
2025-06-01 11:43:03 +0000

    autoload configurations

--- prod/base/entrypoint.sh
+++ prod/base/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
-cd /app && python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+source /etc/environment
+cd /app
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput 
+{
+    python manage.py createsuperuser --noinput
+} || echo "createsuperuser failed, continuing..."
+python manage.py import_initial_data
 exec "$@"
