7f037922e8aaa67485957b43d52aaf8f3eb6114e
edgonzalez <edgonzalez@chacomer.com.py>
2025-06-01 18:27:43 -0300

    feat: renamed fields

--- src/core/api/serializers/book.py
+++ src/core/api/serializers/book.py
@@ -5,11 +5,11 @@ from database.models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     author_names = serializers.SerializerMethodField(read_only=True)
-    authors_count = serializers.IntegerField(read_only=True)
+    num_authors = serializers.IntegerField(read_only=True)
     class Meta:
         model = Book
         fields = (
-            'id', 'title', 'description', 'pub_year', 'genre', 'author_names', 'authors_count'
+            'id', 'title', 'description', 'pub_year', 'genre', 'author_names', 'num_authors'
         )
 
     def get_author_names(self, obj):
